9c68444f135e630aabf17476ce81c01b50a8594c
Константин <osokinkonstantin@mail.ru>
2024-04-28 15:40:49 +0300

    output with two languages

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("привет мир");
         System.out.println("hello world");
+        System.out.println("how are you");
     }
 }
